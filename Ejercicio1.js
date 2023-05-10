1960203995b28392bd989df51e310b763289c26d
Barbi Borrello <biborrello@hotmail.com>
2023-05-10 19:14:28 -0300

    se agregan consignas y ejercicio 1

---
+++ Ejercicio1.js
@@ -0,0 +1,17 @@
+
+let nombre = prompt("¿Cual es tu nombre?");
+alert("¡Hola!" + " " + nombre + " " + "¿como estas?");
+
+
+
+let edad = prompt("¿Cuantos años tenes?");
+alert("Tenes " + " " + edad + " " + "años" + " " + "sos una piba!")
+
+
+
+alert("Calculando resultado...pi pi pi") 
+
+
+  
+let edad_dias = edad * 365;
+alert("Tu nombre es :" + " " + nombre + " " + "y hasta el dia de hoy viviste" + " " + edad_dias + " " + "dias")
